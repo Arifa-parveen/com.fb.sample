07889104fc029229f56a85429a11f0f48e0caca6
Naveen-Ravichandran <naveenravichandran95@gmail.com>
2020-10-08 14:23:49 +0530

    my first commit

---
+++ src/test/java/com/fb/sample/A.java
@@ -0,0 +1,32 @@
+package com.fb.sample;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class A {
+	
+	@BeforeTest
+	public void Setup(){
+		
+	}
+	
+	@Test
+	public void VerifyURL()
+	{
+		
+	}
+	
+	@Test
+	public void VerifyTitle()
+	{
+		
+	}
+	
+	@AfterTest
+	public void Teardown()
+	{
+		
+	}
+
+}
